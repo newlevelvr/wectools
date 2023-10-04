02a65ec19b47fef4f5fcce06559ddf624177ec1f
Ivan Lutsenka <wani4ka@duck.com>
2023-10-04 20:08:23 +0300

    Upgrade to 1.20

--- src/main/java/ru/newlevelvr/wectools/item/Items.java
+++ src/main/java/ru/newlevelvr/wectools/item/Items.java
@@ -1,14 +1,17 @@
 package ru.newlevelvr.wectools.item;
 
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import ru.newlevelvr.wectools.WorldEditColoredTools;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Items {
@@ -21,18 +24,25 @@ public class Items {
             "wooden_pickaxe", "wooden_axe", "wooden_shovel", "wooden_hoe", "stick"
     );
 
+    private static final List<Item> ITEMS = new ArrayList<>();
+
     private static Item wand;
 
     public static Item getWand() {
         return wand;
     }
 
-    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder
-            .build(new Identifier(WorldEditColoredTools.MOD_ID, "tools"), () -> new ItemStack(getWand()));
+    public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
+            .displayName(Text.translatable("itemGroup.wectools.tools"))
+            .icon(() -> new ItemStack(getWand()))
+            .entries((context, entries) -> ITEMS.forEach(entries::add))
+            .build();
 
     private static Item registerTool(String id) {
         var itemId = new Identifier(WorldEditColoredTools.MOD_ID, id);
-        return Registry.register(Registry.ITEM, itemId, new Item(new FabricItemSettings().maxCount(1).group(ITEM_GROUP)));
+        var item = Registry.register(Registries.ITEM, itemId, new Item(new FabricItemSettings().maxCount(1)));
+        ITEMS.add(item);
+        return item;
     }
 
     private static void registerToolGroup(String prefix) {
@@ -44,5 +54,6 @@ public class Items {
     public static void registerTools() {
         TOOLS.forEach(Items::registerToolGroup);
         wand = registerTool("wand");
+        Registry.register(Registries.ITEM_GROUP, new Identifier(WorldEditColoredTools.MOD_ID, "tools"), ITEM_GROUP);
     }
 }
